f01fa36b29504829f261b5e62a950431aaa44fdd
musera16 <museramine@gmail.com>
2023-11-18 05:55:24 -0800

    com

---
+++ Python/day13/lesson13.py
@@ -0,0 +1,10 @@
+#1.What is thse funcion used used to display text Python?
+#print("The text display function in Python is called print")
+
+#2.In Python, which data type is used to store a whole number?
+age = 42
+print(type(age))
+
+
+
+
