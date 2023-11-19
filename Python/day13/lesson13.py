ff344950e015600f0741c465d0c74b58a971b25c
musera16 <museramine@gmail.com>
2023-11-19 06:01:06 -0800

    com

--- Python/day13/lesson13.py
+++ Python/day13/lesson13.py
@@ -1,9 +1,163 @@
 #1.What is thse funcion used used to display text Python?
-#print("The text display function in Python is called print")
+print("The text display function in Python is called print")
 
 #2.In Python, which data type is used to store a whole number?
 age = 42
 print(type(age))
+
+#3.Which of the following data types is used to store text in Python?
+name = "lasha"
+print(type(name))
+
+#4. Which of the following data types is used to store text in Python?
+print(30 + 7 * 3)
+
+#5. Which of the following is the correct way to assign the value 42 to a variable named answer in Python?
+y = 32
+print(y)
+
+#6. What is the process of identifying and fixing errors in a program called?
+#x = y - Error
+x = "y"
+print(x)
+
+#7. Which is commonly used in Python for naming variables and functions, where words are separated by underscore?
+name = "vaxo"
+print(name)
+
+#8. What is the primary purpose of adding comments to your Python code?
+# Writing a code explanation so that the code is not confusing and helps my partner (or anyone who sees the code) understand the code
+
+#9. How can you take user input in Python?
+name = input("your name: ")
+print(name)
+
+#11. What function can be used to check the data type of a variable in Python?
+last_name = "museridze"
+print(type(last_name))
+
+#12. How can you convert an integer to a string in Python?
+age = 35
+str_age = str(age)
+print(str_age)
+print(type(age))
+print(type(str_age))
+
+#13. What is the term for converting one data type into another in Python?
+x = 16
+y = "nika"
+print(x,y)
+print(str(x) + y)
+
+#14. Which operator is used to check if two values are equal in Python?
+print(6 == 6)
+
+#15. What is the result of the logical AND operation between True and False in Python?
+print( 4 == 4) #true
+print( 13 == 8) #false
+
+#16. What will the expression (5 > 3) and (10 < 20) evaluate to in Python?
+print((6 > 4) and (11 < 21)) #true
+
+#17. In Python, what is used to make decisions and execute different code blocks based on conditions?
+if 4 > 2:
+    print("go to GOA")
+else:
+    print("go to spar")
+
+#18. Which type of loop is used to iterate over a sequence (e.g., a list or string) in Python?
+for x in range (3, 13, 5):
+    print(x)
+
+#19. What type of loop is used when you want to repeat a block of code as long as a condition is true?
+x = 1
+while 3 > 2:
+    print(":)")
+    x += 2
+    if x >= 2:
+        break
+
+#20. What does the range() function in Python return?
+x = []
+for i in range(4, 13, 5):
+    x.append(i)
+print(x)
+
+#21. Which keyword is used to start an "if" statement in Python?
+if 5 == 5:
+    print("you teaching in GOA")
+else:
+    print("go to spar")
+
+#22. What is the purpose of the "else" statement in Python's "if-else" construct?
+if 6 == 7:
+    print("nice")
+else:
+    print("bad end")
+
+#23. Which data structure is used to store an ordered collection of items in Python?
+list = [7, 8, 11, 12]
+print(list)
+
+#24. In Python, which index represents the first element of a sequence?
+list = [8, 2, 3, 13]
+print(list, [2])
+
+#25. How can you access the third element of a list in Python?
+list = [7, 5, 8, 16, 19, ]
+print(list[3])
+
+#26. What does slicing allow you to do with a sequence in Python?
+list = [9, 10, 12, 13, 14, ]
+print(list[1:3])
+
+#27. How can you extract a subsequence of a list from index 2 to index 5 (5 must be included) in Python?
+list = [23, 12, 4, 11, 7, ]
+print(list[2:5])
+
+#28. What does the "step" value in slicing indicate?
+for x in range (0, 20, 2):
+                      #^step
+    print(x)
+
+#29. What is a reusable block of code in Python that performs a specific task called?
+def x():
+    print(":D")
+x()
+x()
+x()
+
+#30. In Python, what are the values passed to a function called?
+def x(b):
+   #  ^ argument
+    print(b)
+x(b = 10)
+
+#31. Which string method is used to convert a string to uppercase in Python?
+last_name = "Museridze"
+last_name = last_name.upper()
+print(last_name)
+
+#32.  What list method is used to add an element to the end of a list in Python?
+names = ["lasha", "gega", "oto", "nika", "gio"]
+names.append("oto")
+print(names)
+
+#33. In Python, which keyword is used to define a new function?
+def num(x):
+    print(x)
+num(x = 10)
+
+
+
+ 
+
+
+
+
+
+
+
 
 
 
